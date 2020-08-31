2b1585af69eddccd132474ff160c6c92a0d7e5ae
Pacnysam <69169874+Pacnysam@users.noreply.github.com>
2020-08-30 18:53:05 -0700

    cringe!

---
+++ ArcaneAlchemist.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace ArcaneAlchemist
+{
+    public class ArcaneAlchemist : Mod
+    {
+        public static ArcaneAlchemist instance => ModContent.GetInstance<ArcaneAlchemist>();
+    }
+}
--- ArcaneAlchemist/ArcaneAlchemist.cs
+++
@@ -1,9 +0,0 @@
-using Terraria.ModLoader;
-
-namespace ArcaneAlchemist
-{
-    public class ArcaneAlchemist : Mod
-    {
-        public static ArcaneAlchemist instance => ModContent.GetInstance<ArcaneAlchemist>();
-    }
-}
